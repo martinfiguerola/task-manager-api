5ba7e9741a6273e16d881e92bf9c1eef75a3e765
martinfiguerola <fedefiguerola@gmail.com>
2026-01-16 11:53:29 +0100

    chore: add Repository annotations

--- src/main/java/com/martin/taskmanager/repository/UserRepository.java
+++ src/main/java/com/martin/taskmanager/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.martin.taskmanager.repository;
 
 import com.martin.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
