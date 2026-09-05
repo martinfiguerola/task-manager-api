94585af8a5a0b5bd6845a97ea83c284346631647
martinfiguerola <fedefiguerola@gmail.com>
2026-09-05 13:43:52 +0200

    refactor: replace @NotNull with @NotBlank on description in TaskRequestDTO and TaskUpdateDTO

--- src/main/java/com/martin/taskmanager/dto/task/TaskRequestDTO.java
+++ src/main/java/com/martin/taskmanager/dto/task/TaskRequestDTO.java
@@ -2,7 +2,6 @@ package com.martin.taskmanager.dto.task;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record TaskRequestDTO(
@@ -10,7 +9,7 @@ public record TaskRequestDTO(
         @NotBlank(message = "Title is required") @Size(min = 3, message = "Title must have at least 3 characters")
         String title,
 
-        @NotNull (message = "Description is required") @Size(max = 150, message = "Description cannot exceed 150 characters")
+        @NotBlank (message = "Description is required") @Size(max = 150, message = "Description cannot exceed 150 characters")
         String description
 ) {
 }
